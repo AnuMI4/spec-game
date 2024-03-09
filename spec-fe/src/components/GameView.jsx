f8e59138c6ac30fabc9a4dd0a5d61ad7dee57b71
Anum Mirza <anoom.16@gmail.com>
2024-03-09 22:07:55 +0000

    added card back img

--- spec-fe/src/components/GameView.jsx
+++ spec-fe/src/components/GameView.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import images from './images';
 import '../App.css';
+import cardImage from '../assets/red2.svg';
 
 function GameView() {
   const [clickedCards, setClickedCards] = useState([]);
@@ -16,7 +17,7 @@ function GameView() {
     for (let i = 0; i < 25; i++) {
       gridItems.push(
         <div key={i} className="grid-item" onClick={() => handleCardClick(i)}>
-          {clickedCards.includes(i) ? <img src={images.image4} alt="Card Image" /> : `Card ${i + 1}`}
+          {clickedCards.includes(i) ? <img src={images.image4} alt="Card Image" /> : <img src={cardImage} alt="Card Image" /> }
         </div>
       );
     }
