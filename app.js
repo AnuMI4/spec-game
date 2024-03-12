836934b1d3e054772e6e87eb4966251550924539
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-03-12 15:18:58 +0000

    feat: add 2 player guess in react - wip

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import Game from "./Game.js";
 import Card from "./Card.js";
 import Grid from "./Grid.js";
 import Guess from "./Guess.js";
-import Points from "./Points.js"
+import Points from "./Points.js";
 import Players from "./Players.js";
 import Helper from "./Helper.js";
 const app = express();
@@ -18,13 +18,17 @@ const playersObj = new Players();
 
 app.use(cors());
 
-app.get('/getShowcards', (req, res) => {
-    let deck = cardObj.createShowcardsDeck();
-    cardObj.shuffleDeck(deck);
-    res.send(deck);
+app.get("/getShowcards", (req, res) => {
+  let deck = cardObj.createShowcardsDeck();
+  cardObj.shuffleDeck(deck);
+  res.send(deck);
 });
 
+app.get("/getGuessObj", (req, res) => {
+  let guess = guessObj;
+  res.send(guess);
+});
 
 app.listen(port, () => {
-    console.log('server started on port '+port);
+  console.log("server started on port " + port);
 });
